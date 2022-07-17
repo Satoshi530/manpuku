d3a5248a4c6a7e636ee66ea1d415e59554ae020b
Satoshi530 <satohshi530@gmail.com>
2022-07-17 10:58:34 +0000

    seed変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create!(
-   ENV['ADMINMAIL'],
-   ENV['ADMINPASS']
+   email: ENV['ADMINMAIL'],
+   password: ENV['ADMINPASS']
 )
